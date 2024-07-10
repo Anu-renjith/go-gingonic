523b5621c76be322e60747c341423a89469da3f0
Riyagowri <gowri@fgilservices.com>
2024-07-10 10:23:58 +0530

    service,controller files are added

--- controller/video-controller.go
+++ controller/video-controller.go
@@ -8,15 +8,29 @@ import (
 
 type VideoController interface {
 	FindAll() []entity.Video
-	Save(ctx *gin.Context)
+	Save(ctx *gin.Context) entity.Video
 }
 
 type controller struct {
 	service service.VideoService
 }
 
+// FindAll implements VideoController.
+func (c *controller) FindAll() []entity.Video {
+	return c.service.FindAll()
+}
+
+// Save implements VideoController.
+func (c *controller) Save(ctx *gin.Context) entity.Video {
+	var video entity.Video
+	if err := ctx.BindJSON(&video); err != nil {
+		return entity.Video{} // Handle error properly, maybe log it or return a proper response
+	}
+	return c.service.Save(video)
+}
+
 func New(service service.VideoService) VideoController {
-	return controller{
+	return &controller{
 		service: service,
 	}
 }
--- server.go
+++ server.go
@@ -1,14 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Anu-renjith/go-gingonic/controller"
+	"github.com/Anu-renjith/go-gingonic/service"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	VideoService    service.VideoService    = service.New()
+	VideoController controller.VideoController = controller.New(VideoService)
+)
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/test",func(ctx *gin.Context){
-		ctx.JSON(200,gin.H{
-			"message":"OK!!",
-		})
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, VideoController.FindAll())
 	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, VideoController.Save(ctx))
+	})
+
 	server.Run(":8082")
 }
